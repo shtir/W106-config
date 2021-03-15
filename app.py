825cb784a7252fbc358771ca675800dc5c938028
SHahab <shahab.1369@gmail.com>
2021-03-15 21:25:39 +0330

    change from flask  to waitress

--- app.py
+++ app.py
@@ -55,6 +55,6 @@ def form():
     
     return render_template("form.html", data=data)
 
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=80)    
