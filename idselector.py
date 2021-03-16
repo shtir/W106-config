84965eec0532423e6b3f5a9cd09bcdb9fb740db1
SHahab <shahab.1369@gmail.com>
2021-03-16 17:45:52 +0330

    simple bug

--- idselector.py
+++ idselector.py
@@ -1,46 +1,47 @@
-def idselector()
-    def __init__(self,id)
-    if (id == "1"):
+class idselector:
+
+    def __init__(self,id):
+        if (id == 1):
             self.ipAddress = "192.168.88.10"
             self.port = 8080
             self.dbname = "H1V1A01"
-        if (id == "2"):
+        if (id == 2):
             self.ipAddress = "192.168.88.11"
             self.port = 8080
             self.dbname = "F1V1A01"
-        if (id == "3"):
+        if (id == 3):
             self.ipAddress = "192.168.88.12"
             self.port = 8080
             self.dbname = "A1V1A02"
-        if (id == "4"):
+        if (id == 4):
             self.ipAddress = "192.168.88.13"
             self.port = 8080
             self.dbname = "A1V1A01"
-        if (id == "5"):
+        if (id == 5):
             self.ipAddress = "192.168.88.14"
             self.port = 8080
             self.dbname = "D1V1A01"
-        if (id == "6"):
+        if (id == 6):
             self.ipAddress = "192.168.88.15"
             self.port = 8080
             self.dbname = "MainLab1"
-        if (id == "7"):
+        if (id == 7):
             self.ipAddress = "192.168.88.16"
             self.port = 8080
             self.dbname = "MainLab2"
-        if (id == "8"):
+        if (id == 8):
             self.ipAddress = "192.168.88.30"
             self.port = 8080
             self.dbname = "Diesel"
-        if (id == "9"):
+        if (id == 9):
             self.ipAddress = "192.168.88.31"
             self.port = 8080
             self.dbname = "B1"
-        if (id == "10"):
+        if (id == 10):
             self.ipAddress = "192.168.88.32"
             self.port = 8080
             self.dbname = "B2"
-        if (id == "11"):
+        if (id == 11):
             self.ipAddress = "192.168.88.33"
             self.port = 8080
             self.dbname = "B3"
