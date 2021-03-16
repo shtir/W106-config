9f0f81d7572e93400496407725a34649a46b63c3
SHahab <shahab.1369@gmail.com>
2021-03-16 17:15:49 +0330

    remove an empty if

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from pyModbusTCP.client import ModbusClient
+from idselector import *
 
 class W106:
 
@@ -9,33 +10,33 @@ class W106:
         c.port(port)
         c.unit_id(1)
         c.open()
-        if (True):
-            data = c.read_holding_registers(130, 12)
-            self.data=data
-            c.close()
-            if data:
-                self.LowT1Start = format(data[0], 'x')
-                self.LowT1Stop = format(data[1], 'x')
-                self.LowT2Start = format(data[2], 'x')
-                self.LowT2Stop = format(data[3], 'x')
-                
-                self.NormT1Start = format(data[4], 'x')
-                self.NormT1Stop = format(data[5], 'x')
-                self.NormT2Start = format(data[6], 'x')
-                self.NormT2Stop = format(data[7], 'x')
-
-                self.PeakT1Start = format(data[8], 'x')
-                self.PeakT1Stop = format(data[9], 'x')
-                self.PeakT2Start = format(data[10], 'x')
-                self.PeakT2Stop = format(data[11], 'x')
-
-            else:
-                print("Read Volt And Amper ERROR")
+        data = c.read_holding_registers(130, 12)
+        self.data=data
+        c.close()
+        if data:
+            self.LowT1Start = format(data[0], 'x')
+            self.LowT1Stop = format(data[1], 'x')
+            self.LowT2Start = format(data[2], 'x')
+            self.LowT2Stop = format(data[3], 'x')
+            
+            self.NormT1Start = format(data[4], 'x')
+            self.NormT1Stop = format(data[5], 'x')
+            self.NormT2Start = format(data[6], 'x')
+            self.NormT2Stop = format(data[7], 'x')
+            self.PeakT1Start = format(data[8], 'x')
+            self.PeakT1Stop = format(data[9], 'x')
+            self.PeakT2Start = format(data[10], 'x')
+            self.PeakT2Stop = format(data[11], 'x')
+        else:
+            print("Read Volt And Amper ERROR")
                 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    id = request.args.get("id")
+    if (id):
+        idselector(id)
     #data=W106("192.168.88.15", 8080)
     print(request.args.get("name"))
     data = ""
